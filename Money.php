96f7db9cb6f302882acfa960301b7e777be9b1a2
Aman <100346282+aman-studio@users.noreply.github.com>
2022-04-05 16:52:32 +0530

    Add files via upload

---
+++ Money.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://www.freepnglogos.com/uploads/avengers-png-logo/black-avengers-png-logo-2.png" rel="icon" type="image/x-icon"/>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/ak47.css">
+<title>Aman 47 [ Script ]</title>
+</head>
+<body>
+<div class="limiter">
+<h1>5Star Script</h1>
+<div class="frame"><iframe width="99%" height="800px" src="https://jb99.cf/5s/" allow="autoplay; encrypted-media" allowfullscreen="yes" scrolling="no"></iframe></div>
+
+
+
+<div class="telegram">
+<i class="fa fa-telegram" aria-hidden="true"></i>
+<a href="https://telegram.me/secureerror">Join Our Telegram</a>
+</div>
+<div class="telegram">
+<i class="fa fa-youtube" aria-hidden="true"></i>
+<a href="https://bit.ly/3tNAnG0">Subscribe Our YouTube</a>
+</div>
+</div>
+</div><br>
+</body>
+</html>
